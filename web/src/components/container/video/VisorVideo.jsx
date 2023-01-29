ce1db05bfc9816f833427ad067ba3976a926e171
orodriguez2015 <oscar.rodriguezbrea@gmail.com>
2023-01-29 23:59:05 +0100

    Visor de vídeo
    Se modifica el tamaño y el aspecto de la miniatura del vídeo a formato 4:3

--- web/src/components/container/video/VisorVideo.jsx
+++ web/src/components/container/video/VisorVideo.jsx
@@ -15,17 +15,23 @@ class VisorVideo extends React.Component {
         let rutaVideo = URL_BACKEND_VIDEOS + this.props.video.rutaRelativa;
         const autoPlay = false;
         const playsInline = true;
-        const fluid = true;
+        const fluid = false;
         const preload = "metadata";
+        const width = 200;
+        const height = 190;
+        const aspectRatio = "4:3";
 
         return (
-            <div>
+            <div align="center">
                 <Player
                     autoPlay = {autoPlay}
                     playsInline = {playsInline}
                     fluid = {fluid}
                     preload = {preload}
-                    src={rutaVideo}/>
+                    src={rutaVideo}
+                    witdh= {width}
+                    height={height}
+                    aspectRatio={aspectRatio}/>
             </div>
         );
     }
